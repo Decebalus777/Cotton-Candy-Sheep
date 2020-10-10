741a07c0a7e40ce09210a00eb66cacacdca3410b
MjGalicha <mjgalicha@gmail.com>
2020-10-09 22:24:00 -0600

    Added ability to pick up and change color

--- Cotton Candy Sheep Project/Assets/PlayerController.cs
+++ Cotton Candy Sheep Project/Assets/PlayerController.cs
@@ -6,10 +6,20 @@ public class PlayerController : MonoBehaviour
 {
     public int movementSpeed = 10;
     public GameObject currentCloak;
+    public GameObject potentialCloak;
+    public GameObject pickedUpCloak;
+    public bool pickup = false;
+
+    Material baseMaterial;
+    Material potentialMaterial;
+    Material currentMaterial;
+    Material pickedupMaterial;
     // Start is called before the first frame update
     void Start()
     {
-        
+        baseMaterial = gameObject.GetComponent<MeshRenderer>().material;
+        currentMaterial = baseMaterial;
+        gameObject.GetComponent<MeshRenderer>().material = currentMaterial;
     }
 
     // Update is called once per frame
@@ -37,13 +47,54 @@ public class PlayerController : MonoBehaviour
         {
             transform.position += Vector3.right * movementSpeed * Time.deltaTime;
         }
+        if(Input.GetKeyDown("j"))
+        {
+            Pickup();
+        }
+        if(Input.GetKeyDown("k"))
+        {
+            UseItem();
+        }
+    }
+    void UseItem()
+    {
+        if(pickedupMaterial != null)
+        {
+            currentCloak = pickedUpCloak;
+            pickedUpCloak = null;
+            gameObject.GetComponent<MeshRenderer>().material = pickedupMaterial;
+            pickedupMaterial = null;
+        }
+    }
+
+    void Pickup()
+    {
+        if(pickup && potentialCloak != null)
+        {
+            pickedUpCloak = potentialCloak;
+            pickedupMaterial = potentialMaterial;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.CompareTag("Pickup"))
+        if (other.gameObject.tag == "Pickup")
         {
-            Debug.Log("Item Picked up");
-        }
+            pickup = true;
+            potentialCloak = other.gameObject;
+            potentialMaterial = other.gameObject.GetComponent<MeshRenderer>().material;
+        }   
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Pickup")
+        {
+            pickup = false;
+            potentialCloak = null;
+            potentialMaterial = null;
+        }   
+    }
+
+    
 }
