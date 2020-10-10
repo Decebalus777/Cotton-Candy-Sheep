df4675368957cdc80b52ff73c7c1c1fabe538865
MjGalicha <mjgalicha@gmail.com>
2020-10-09 21:22:25 -0600

    Setup camera and player movement

---
+++ Cotton Candy Sheep Project/Assets/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int movementSpeed = 10;
+    public GameObject currentCloak;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+
+        if(Input.GetKey("w"))
+        {
+            transform.position += Vector3.forward * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position += Vector3.back * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Pickup"))
+        {
+            Debug.Log("Item Picked up");
+        }
+    }
+}
