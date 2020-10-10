b613b43d0a81dd13296ce5249b0e934833921ee8
MjGalicha <mjgalicha@gmail.com>
2020-10-09 23:25:34 -0600

    Made color reset after time limit

---
+++ Cotton Candy Sheep Project/Assets/Pickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public bool pickedUp = false;
+
+    private void FixedUpdate()
+    {
+        if (pickedUp)
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+            gameObject.GetComponent<Collider>().enabled = false;
+
+        }
+
+        if (!pickedUp)
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+            gameObject.GetComponent<Collider>().enabled = true;
+        }
+    }
+}
--- Cotton Candy Sheep Project/Assets/PlayerController.cs
+++ Cotton Candy Sheep Project/Assets/PlayerController.cs
@@ -5,21 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public int movementSpeed = 10;
+    public int destroyTime = 30;
+
+    //public for debugging purposes
     public GameObject currentCloak;
     public GameObject potentialCloak;
     public GameObject pickedUpCloak;
+    public GameObject toUseCloak;
     public bool pickup = false;
+    
 
+    //placeholder materials
     Material baseMaterial;
     Material potentialMaterial;
-    Material currentMaterial;
     Material pickedupMaterial;
     // Start is called before the first frame update
     void Start()
     {
         baseMaterial = gameObject.GetComponent<MeshRenderer>().material;
-        currentMaterial = baseMaterial;
-        gameObject.GetComponent<MeshRenderer>().material = currentMaterial;
     }
 
     // Update is called once per frame
@@ -56,26 +59,61 @@ public class PlayerController : MonoBehaviour
             UseItem();
         }
     }
+
+    //Takes the picked up cloak and sets it to the current cloak of player
     void UseItem()
     {
         if(pickedupMaterial != null)
         {
             currentCloak = pickedUpCloak;
+            Destroy(toUseCloak);
             pickedUpCloak = null;
             gameObject.GetComponent<MeshRenderer>().material = pickedupMaterial;
             pickedupMaterial = null;
         }
+
+        StartCoroutine(WaitThenDie());
     }
 
+    //Grabs which ever cloak player is hovering over
     void Pickup()
     {
-        if(pickup && potentialCloak != null)
+        if(pickup)
         {
-            pickedUpCloak = potentialCloak;
-            pickedupMaterial = potentialMaterial;
+            //Destroys the cloak that the player interacts with and sets the new cloak to use
+            if (potentialCloak != null)
+            {
+                pickedUpCloak = potentialCloak;
+                Destroy(potentialCloak);
+                potentialCloak = null;
+                pickedupMaterial = potentialMaterial;
+            }
+
+            //checks if there is no next material to change into creates a new instance of that material
+            if (pickedUpCloak != null && toUseCloak == null)
+            {
+                toUseCloak = Instantiate(pickedUpCloak, transform.position + Vector3.up, Quaternion.identity);
+                toUseCloak.transform.parent = gameObject.transform;
+            }
+            //if player already has a cloak they can use, destroys that and sets it as the new one
+            else if (toUseCloak != null)
+            {
+                Destroy(toUseCloak);
+                toUseCloak = null;
+                toUseCloak = Instantiate(pickedUpCloak, transform.position + Vector3.up, Quaternion.identity);
+                toUseCloak.transform.parent = gameObject.transform;
+            }
         }
+        pickup = false;
+    }
+    //timer for when materials change
+    IEnumerator WaitThenDie()
+    {
+        yield return new WaitForSeconds(destroyTime);
+        gameObject.GetComponent<Renderer>().material = baseMaterial;
     }
 
+    //Checks for any Cloaks player advances to
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Pickup")
@@ -86,6 +124,7 @@ public class PlayerController : MonoBehaviour
         }   
     }
 
+    //resets options for picking up a new cloak
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.tag == "Pickup")
@@ -94,7 +133,5 @@ public class PlayerController : MonoBehaviour
             potentialCloak = null;
             potentialMaterial = null;
         }   
-    }
-
-    
+    } 
 }
